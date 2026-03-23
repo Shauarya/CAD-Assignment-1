91bb40e7b4718a0b79f983bbc99a2144ee5daa0a
saurya verma <shauaryaverna@gmail.com>
2026-03-22 21:25:34 -0700

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,18 @@
+student = {
+    "name": "saurya",
+    "age": 20,
+    "course": "BCA"
+}
+
+ 
+print("Name:", student["name"])
+print("Age:", student["age"])
+
+ 
+student["city"] = "Lucknow"
+
+ 
+student["age"] = 21
+
+ 
+print("Updated Dictionary:", student)
