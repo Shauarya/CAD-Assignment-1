d1e61d5779002b368363a413f0f410d21b06421b
saurya verma <shauaryaverna@gmail.com>
2026-03-22 21:35:09 -0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# Function to display tuple elements
+def show_tuple(data):
+    for item in data:
+        print(item)
+
+# Creating a tuple
+numbers = (10, 20, 30, 40, 50)
+
+# Calling function
+show_tuple(numbers)
