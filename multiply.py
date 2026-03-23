3cf97629b1e050160d11bdbc7f91378696cee58f
saurya verma <shauaryaverna@gmail.com>
2026-03-22 21:10:49 -0700

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,13 @@
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calling function
+result = multiply(num1, num2)
+
+# Printing result
+print("Multiplication is:", result)
